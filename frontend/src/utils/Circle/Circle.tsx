8c1a83ce75d2ad965fa9871fc984119d9c2396e2
Andrew Amadeo <andrewamadeo411@gmail.com>
2025-10-15 15:05:20 -0400

    Added smaller circle style

--- frontend/src/utils/Circle/Circle.tsx
+++ frontend/src/utils/Circle/Circle.tsx
@@ -1,19 +1,13 @@
+import SmallCircle from '../SmallCircle/SmallCircle'
 import './Circle.css'
 
 const Circle: React.FC = () => {
 
-    // const circleStyle: React.CSSProperties = {
-    //     width: 100,
-    //     height: 100,
-    //     backgroundColor: 'black',
-    //     borderRadius: '50%',
-    // }
-
     return (
         <div
             className='circle'
-        // style={circleStyle}
         >
+            <SmallCircle />
         </div>
     )
 }
---
+++ frontend/src/utils/SmallCircle/SmallCircle.tsx
@@ -0,0 +1,12 @@
+import './SmallCircle.css'
+
+const SmallCircle: React.FC = () => {
+
+    return (
+        <div
+            className="small-circle"
+        />
+    )
+}
+
+export default SmallCircle
