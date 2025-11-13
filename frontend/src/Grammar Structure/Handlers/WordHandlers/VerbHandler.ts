3acd1d6963ccb8914f8b615ae85e62fa645262a5
Andrew Amadeo <andrewamadeo411@gmail.com>
2025-11-13 16:51:00 -0500

    Replaced if/else if with a Map

--- frontend/src/Grammar Structure/Handlers/WordHandlers/VerbHandler.ts
+++ frontend/src/Grammar Structure/Handlers/WordHandlers/VerbHandler.ts
@@ -1,61 +1,94 @@
 import type { WordHandler } from "./WordHandler"
 import type { Word } from "../../types/Word"
 import { ClauseBuilder } from "../../syntax/ClauseBuilder"
-import { isECMPred, isObjectControlPred, isRaisingPred, isRosVerb } from "../../syntax/SyntaxMethods"
+import { isECMPred, isObjectControlPred, isRaisingPred }
+    from "../../syntax/SyntaxMethods"
 import { Predicate } from "../../syntax/Predicate"
 import type { Noun } from "../../syntax/partsOfSpeech/Noun"
 import type { Clause } from "../../syntax/partsOfSpeech/Clause"
 
-export class VerbHandler implements WordHandler {
+enum PredType {
+    ECM,
+    OCONTROL,
+    RAISING,
+    SCONTROL
+}
 
-    private shouldStartNewClause(pred: Predicate | null): boolean {
-        if (!(pred instanceof Predicate)) {
-            return false
-        }
-        return (
-            isECMPred(pred) ||
-            isRosVerb(pred)
-        )
-    }
+export class VerbHandler implements WordHandler {
 
     public handle(
         verbalWord: Word,
         clauseBuilder: ClauseBuilder
     ): ClauseBuilder | void {
         let pred: Predicate | null = clauseBuilder.getPredicate()
-        if (pred instanceof Predicate) {
-            if (isECMPred(pred)) {
-                let subSubject: Noun = clauseBuilder.yieldEcmNoun()
-                let mtxClause: Clause = clauseBuilder.build()
-                console.log(mtxClause)
-                let subClause: ClauseBuilder = new ClauseBuilder()
-                subClause.receiveSubject(subSubject)
-                subClause.buildPredicate(verbalWord)
-                return subClause
-            } else if (isObjectControlPred(pred)) {
-                let subSubject: Noun = clauseBuilder.yeildOControlNoun()
-                let mtxClause: Clause = clauseBuilder.build()
-                console.log(mtxClause)
-                let subClause: ClauseBuilder = new ClauseBuilder()
-                subClause.receiveSubject(subSubject)
-                return subClause
-            } else if (isRaisingPred(pred)) {
-                let subSubject: Noun = clauseBuilder.yieldRaisingNoun()
-                let mtxClause: Clause = clauseBuilder.build()
-                console.log(mtxClause)
-                let subClause: ClauseBuilder = new ClauseBuilder()
-                subClause.receiveSubject(subSubject)
-                return subClause
-            } else {
-                let subSubject: Noun = clauseBuilder.yieldSControlNoun()
-                let mtxClause: Clause = clauseBuilder.build()
-                console.log(mtxClause)
-                let subClause: ClauseBuilder = new ClauseBuilder()
-                subClause.receiveSubject(subSubject)
-                return subClause
-            }
+        if (pred) {
+            let action = this.getPredAction(pred)
+            return action(verbalWord, clauseBuilder)
         } else {
             clauseBuilder.buildPredicate(verbalWord)
         }
+    }
+
+    private handleECM(vWord: Word, cBuilder: ClauseBuilder): ClauseBuilder {
+        let subSubject: Noun = cBuilder.yieldEcmNoun()
+        let mtxClause: Clause = cBuilder.build()
+        console.log(mtxClause)
+        let subClause: ClauseBuilder = new ClauseBuilder()
+        subClause.receiveSubject(subSubject)
+        subClause.buildPredicate(vWord)
+        return subClause
+    }
+
+    private handleOControl(cBuilder: ClauseBuilder): ClauseBuilder {
+        let subSubject: Noun = cBuilder.yeildOControlNoun()
+        let mtxClause: Clause = cBuilder.build()
+        console.log(mtxClause)
+        let subClause: ClauseBuilder = new ClauseBuilder()
+        subClause.receiveSubject(subSubject)
+        return subClause
+    }
+
+    private handleRaising(cBuilder: ClauseBuilder): ClauseBuilder {
+        let subSubject: Noun = cBuilder.yieldRaisingNoun()
+        let mtxClause: Clause = cBuilder.build()
+        console.log(mtxClause)
+        let subClause: ClauseBuilder = new ClauseBuilder()
+        subClause.receiveSubject(subSubject)
+        return subClause
+    }
+
+    private handleSControl(cBuilder: ClauseBuilder): ClauseBuilder {
+        let subSubject: Noun = cBuilder.yieldSControlNoun()
+        let mtxClause: Clause = cBuilder.build()
+        console.log(mtxClause)
+        let subClause: ClauseBuilder = new ClauseBuilder()
+        subClause.receiveSubject(subSubject)
+        return subClause
+    }
+
+    private getPredType(pred: Predicate): PredType {
+        if (isECMPred(pred)) return PredType.ECM
+        if (isObjectControlPred(pred)) return PredType.OCONTROL
+        if (isRaisingPred(pred)) return PredType.RAISING
+        else return PredType.SCONTROL
+    }
+
+    private getPredAction(pred: Predicate) {
+        let controlMap = new Map([
+            [PredType.ECM, (verb: Word, clause: ClauseBuilder) =>
+                this.handleECM(verb, clause)],
+            [PredType.OCONTROL, (_verb: Word, clause: ClauseBuilder) =>
+                this.handleOControl(clause)],
+            [PredType.RAISING, (_verb: Word, clause: ClauseBuilder) =>
+                this.handleRaising(clause)],
+            [PredType.SCONTROL, (_verb: Word, clause: ClauseBuilder) =>
+                this.handleSControl(clause)],
+        ])
+        let pType: PredType = this.getPredType(pred)
+        let vHandler = controlMap.get(pType)
+        if (!vHandler) {
+            throw Error("Unable to get proper handler for main verb.")
+        }
+        return vHandler
     }
 }
