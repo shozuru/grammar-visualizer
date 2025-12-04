0131dbd771b49cab90493ae213718c4795e3ef4b
Andrew Amadeo <andrewamadeo411@gmail.com>
2025-12-04 14:45:35 -0500

    changed line spacing

--- frontend/src/grammar-structure/handlers/word-handlers/adjective-handler.ts
+++ frontend/src/grammar-structure/handlers/word-handlers/adjective-handler.ts
@@ -15,15 +15,10 @@ export class AdjectiveHandler implements WordHandler {
             builder.buildAdjective(adjWord)
             return
         }
-
-        ctx.push(builder)
-        // add this to the list of clauses using handlerMethods
-        return builder.buildAdjRelClause(adjWord)
-        // copy the last noun builder and put it into the relative clause's noun list (to get determiners and such)
-        // when we get to the next word, we want to add that noun to both the relative clause and the main clause
-
         // This is a red balloon (this is a balloon that is red)
         // This is a birthday cake (this is a cake that is X a birthday)
+        ctx.push(builder)
+        return builder.buildAdjRelClause(adjWord)
     }
 
     private isPredicateAdj(cBuilder: ClauseBuilder): boolean {
