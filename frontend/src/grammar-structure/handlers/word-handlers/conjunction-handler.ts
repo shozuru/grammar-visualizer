c35e8207849d653cf10687ef775c060d4ba49bcb
Andrew Amadeo <andrewamadeo411@gmail.com>
2026-01-04 09:34:43 -0500

    added more basic logic for handling conjunctions

--- frontend/src/grammar-structure/handlers/word-handlers/conjunction-handler.ts
+++ frontend/src/grammar-structure/handlers/word-handlers/conjunction-handler.ts
@@ -1,9 +1,19 @@
-import type { ClauseBuilder } from "../../builders/clause-builder";
+import { ClauseBuilder } from "../../builders/clause-builder";
+import type { HandlerMethods } from "../../parser";
+import type { Clause } from "../../syntax/parts-of-speech/clause";
 import type { Word } from "../../types/word";
 import type { WordHandler } from "./word-handler";
 
 export class ConjunctionHandler implements WordHandler {
-    public handle(conjunction: Word, builder: ClauseBuilder): void {
-        throw Error("I made it this far")
+
+    public handle(
+        conjunction: Word,
+        cBuilder: ClauseBuilder,
+        ctx: HandlerMethods
+    ): ClauseBuilder | void {
+        const completedClause: Clause = cBuilder.build()
+        ctx.add(completedClause)
+        const newClauseBuilder: ClauseBuilder = new ClauseBuilder()
+        return newClauseBuilder
     }
 }
